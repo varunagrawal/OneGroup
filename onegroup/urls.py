26be32dac62e2848c06eba5192184b55da5c8d29
Varun Agrawal <varagrawal@gmail.com>
2016-07-14 13:52:39 -0400

    bugfix for homepage

--- onegroup/urls.py
+++ onegroup/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-import onedrive
-import cognitive
+from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^/', views.index, name="index"),
+    url(r'^', views.index, name="index"),
     url(r'^onedrive/', include('onedrive.urls')),
 ]
